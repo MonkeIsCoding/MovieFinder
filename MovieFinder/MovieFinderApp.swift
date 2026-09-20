43b0deea06868820b207433233c18a708c25fc60
MonkeIsCoding <franciscocdsousa@icloud.com>
2026-09-20 22:42:28 +0100

    Initial Commit

---
+++ MovieFinder/MovieFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFinderApp.swift
+//  MovieFinder
+//
+//  Created by Kiko on 20/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
